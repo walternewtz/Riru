00b58c5df9a18ce616564c9a5cd2720a45f3212d
Alhyoss <simon.lardinois@hotmail.com>
2022-10-18 14:18:02 +0200

    Fixing filename of libraries

--- template/update_package/zip/update.sh
+++ template/update_package/zip/update.sh
@@ -30,13 +30,13 @@ mkdir -p $RIRUDIR/system/lib
 mkdir -p $RIRUDIR/system/lib64
 
 add_file $FILESDIR/lib/armeabi-v7a/libriru.so $RIRUDIR/lib/libriru.so
-add_file $FILESDIR/lib/arm64-v8a/libriru64.so $RIRUDIR/lib64/libriru64.so
+add_file $FILESDIR/lib/arm64-v8a/libriru.so $RIRUDIR/lib64/libriru.so
 add_file $FILESDIR/lib/armeabi-v7a/libriruhide.so $RIRUDIR/lib/libriruhide.so
-add_file $FILESDIR/lib/arm64-v8a/libriruhide64.so $RIRUDIR/lib64/libriruhide64.so
+add_file $FILESDIR/lib/arm64-v8a/libriruhide.so $RIRUDIR/lib64/libriruhide.so
 add_file $FILESDIR/lib/armeabi-v7a/libriruloader.so $RIRUDIR/system/lib/libriruloader.so
-add_file $FILESDIR/lib/arm64-v8a/libriruloader64.so $RIRUDIR/system/lib64/libriruloader64.so
+add_file $FILESDIR/lib/arm64-v8a/libriruloader.so $RIRUDIR/system/lib64/libriruloader.so
 add_file $FILESDIR/lib/armeabi-v7a/libriruloader.so /system/lib/libriruloader.so
-add_file $FILESDIR/lib/arm64-v8a/libriruloader64.so /system/lib64/libriruloader64.so
+add_file $FILESDIR/lib/arm64-v8a/libriruloader.so /system/lib64/libriruloader.so
 add_file $FILESDIR/rirud.apk $RIRUDIR/rirud.apk
 add_file $FILESDIR/rirud_launcher.rc /system/etc/init/rirud_launcher.rc
 
