3fa555d01cb6a19238adcc34df68c7916c039aa4
残页 <31466456+canyie@users.noreply.github.com>
2023-01-05 12:03:51 +0800

    Fix typo

--- template/magisk_module/post-fs-data.sh
+++ template/magisk_module/post-fs-data.sh
@@ -11,19 +11,19 @@ MODPROP="$MODDIR/module.prop"
 #fi
 #mount --bind "$TMPPROP" "$MIRRORPROP"
 if [ "$ZYGISK_ENABLE" = "1" ]; then
-    sed -Ei 's/^description=(\[.*][[:space:]]*)?/description=[ ⛔ Riru is not loaded because of Zygisk. ] /g' "$MIRRORPROP"
+    sed -Ei 's/^description=(\[.*][[:space:]]*)?/description=[ ⛔ Riru is not loaded because of Zygisk. ] /g' "$MODPROP"
     exit
 fi
 
 # KernelSU does not support custom sepolicy patches yet
 ./magiskpolicy --live --apply "$MODDIR/sepolicy.rule"
 
-sed -Ei 's/^description=(\[.*][[:space:]]*)?/description=[ ⛔ app_process fails to run. ] /g' "$MIRRORPROP"
+sed -Ei 's/^description=(\[.*][[:space:]]*)?/description=[ ⛔ app_process fails to run. ] /g' "$MODPROP"
 cd "$MODDIR" || exit
 #flock "module.prop"
 #mount --bind "$TMPPROP" "$MODDIR/module.prop"
 export PATH="$PATH:$MODDIR"
-# Rirud must be startd before ro.dalvik.vm.native.bridge being reset
+# Rirud must be started before ro.dalvik.vm.native.bridge being reset
 unshare -m sh -c "/system/bin/app_process -Djava.class.path=rirud.apk /system/bin --nice-name=rirud riru.Daemon 25206 $MODULESDIR $(getprop ro.dalvik.vm.native.bridge)&"
 #umount "$MODDIR/module.prop"
 
